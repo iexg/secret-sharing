f1d0ae3eb913631d879e310753c6df126591cd1f
James Cape <james.cape@iextrading.com>
2018-04-09 13:17:11 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='secretsharing',
-    version='0.2.6',
+    version='0.2.7',
     url='https://github.com/onenameio/secret-sharing',
     license='MIT',
     author='Halfmoon Labs',
